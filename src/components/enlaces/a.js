92325f21aab55f54de9774e203fdce1afd68a7fb
lautaro-developer <altairdiaz245@gmail.com>
2022-12-27 17:01:32 -0300

    Cositas

--- src/components/enlaces/a.js
+++ src/components/enlaces/a.js
@@ -1,6 +1,18 @@
-export const Enlace = ({ href, valor, id, target }) => {
-  return (
-    <a key={id} href={href} target={target}>
+/**
+ *
+ * @href "Direccion web o de seciones"
+ * @valor "Contenido del elemento"
+ * @id "La key (Se indentifica con un valor unico, por el valor)"
+ * @target "Para abrir una pagina en otra pestaña, (Este usa una operacion ternaria por si target={true} te envia a otra pagina, por defecto esta en false)"
+ * @returns
+ */
+export const Enlace = ({ href = "#", valor, id, target = false }) => {
+  return target ? (
+    <a key={id} href={href} target="_blank" rel="noreferrer">
+      {valor}
+    </a>
+  ) : (
+    <a key={id} href={href}>
       {valor}
     </a>
   );
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { Enlace } from "../enlaces/a";
 
-import "../../css/footer/ul__footer/ul__footer.css";
+import "../../css/footer/div__footer/div__footer.css";
 
 const Iconos = ({ icon, size }) => {
   <FontAwesomeIcon icon={icon} size={size} />;
@@ -21,7 +21,15 @@ export default function Footer() {
   return (
     <footer>
       <div className="div__footer">
-        <p>Si queres ver el codigo esta en</p> {<Enlace valor="Github"/>}
+        <p>
+          Si queres ver el codigo esta en{" "}
+          {<Enlace href={github} valor="Github" target={true} />}. Si queres
+          contactarme aqui te dejo mis redes{" "}
+          {<Enlace href={tw} valor="Twitter" target={true} />} o si queres por{" "}
+          {<Enlace href="as" valor="Linkedin" target={true} />}
+        </p>
+
+        <p></p>
       </div>
     </footer>
   );
